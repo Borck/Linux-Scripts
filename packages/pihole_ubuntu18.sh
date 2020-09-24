7ea6663b90d9261c98f37e5915580e529f9a5dce
Christian Borck <borck@hotmail.de>
2020-09-24 23:25:53 +0200

    [BUGFIX] fix pihole installation to run twice

--- packages/pihole_ubuntu18.sh
+++ packages/pihole_ubuntu18.sh
@@ -2,8 +2,6 @@
 
 sudo add-apt-repository universe
 curl -sSL https://install.pi-hole.net | bash
-wget -O basic-install.sh https://install.pi-hole.net
-sudo bash basic-install.sh
 
 # change password
 pihole -a -p
